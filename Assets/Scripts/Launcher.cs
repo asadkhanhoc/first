9dddc3b606f79d989c132860290f2dcb3f4e4c44
asadkhanhoc <asadkhanhoc@gmail.com>
2024-09-20 23:09:15 +0500

    project start

---
+++ Assets/Scripts/Launcher.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using TMPro;
+
+public class Launcher : MonoBehaviourPunCallbacks
+{
+    public static Launcher instance;
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public GameObject loadingScreen;
+    public TMP_Text loadingText;
+
+    public GameObject menuButtons;
+
+    public GameObject createRoomScreen;
+    public TMP_InputField roomNameInput;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CloseMenus();
+
+        loadingScreen.SetActive(true);
+        loadingText.text = "Connecting To Network...";
+
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    void CloseMenus()
+    {
+        loadingScreen.SetActive(false);
+        menuButtons.SetActive(false);
+        createRoomScreen.SetActive(false);
+    }
+
+    public override void OnConnectedToMaster()
+    {
+
+        PhotonNetwork.JoinLobby();
+
+        loadingText.text = "Joining Lobby...";
+    }
+
+    public override void OnJoinedLobby()
+    {
+        CloseMenus();
+        menuButtons.SetActive(true);
+    }
+
+    public void OpenRoomCreate()
+    {
+        CloseMenus();
+        createRoomScreen.SetActive(true);
+    }
+}
